ec553b35a66bbbcd76f67dc75cdcfe3ab06d02d3
Aguinaldo Silva <aguinaldooo.ecio@gmail.com>
2025-06-17 00:47:12 -0300

    fix/adjust vercel.json

--- src/main.ts
+++ src/main.ts
@@ -6,34 +6,39 @@ import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { getAbsoluteFSPath } from 'swagger-ui-dist';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, //remove propriedades que não estão no DTO
-    forbidNonWhitelisted: true, //retorna um erro se houver propriedades que não estão no DTO
-    transform: true, //transforma o tipo da propriedade para o tipo do DTO
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }),
     new parseIntIdPipe()
-
   );
 
+
+  const docsPath = 'docs';
+
   app.useStaticAssets(getAbsoluteFSPath(), {
-    prefix: '/api',
+    prefix: `/${docsPath}`,
   });
 
+
   if (process.env.NODE_ENV === 'production') {
-  app.use(helmet({
+    app.use(helmet());
 
-  }));
-
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:4200'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    app.enableCors({
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:4200',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true,
     });
   }
+
 
   const documentBuilder = new DocumentBuilder()
     .setTitle('Recados API')
@@ -43,8 +48,11 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, documentBuilder);
-  SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.APP_PORT ?? 3000);
+
+  SwaggerModule.setup(docsPath, app, document);
+
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
