1f1bbf505f6fdb96c5bb5872577037f4ddac08ff
Aguinaldo Silva <aguinaldooo.ecio@gmail.com>
2025-06-17 01:03:22 -0300

    fix/adjust vercel.json

--- src/main.ts
+++ src/main.ts
@@ -4,43 +4,29 @@ import { ValidationPipe } from '@nestjs/common';
 import { parseIntIdPipe } from './common/pipes/parseIntId.pipe';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { getAbsoluteFSPath } from 'swagger-ui-dist';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
+    whitelist: true, //remove propriedades que não estão no DTO
+    forbidNonWhitelisted: true, //retorna um erro se houver propriedades que não estão no DTO
+    transform: true, //transforma o tipo da propriedade para o tipo do DTO
   }),
     new parseIntIdPipe()
+
   );
 
-
-  const docsPath = 'docs';
-
-  app.useStaticAssets(getAbsoluteFSPath(), {
-    prefix: `/${docsPath}`,
-  });
-
-
   if (process.env.NODE_ENV === 'production') {
-    app.use(helmet({
-      contentSecurityPolicy: false,
-    }));
+  app.use(helmet({
+    contentSecurityPolicy: false,
+  }));
 
-    app.enableCors({
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:4200',
-      ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:4200'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true,
     });
   }
-
 
   const documentBuilder = new DocumentBuilder()
     .setTitle('Recados API')
@@ -50,11 +36,8 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api', app, document);
 
-
-  SwaggerModule.setup(docsPath, app, document);
-
-
-  await app.listen(process.env.DATABASE_PORT || 3000);
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
